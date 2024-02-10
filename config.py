d0263b2888934c8c82d5ccfe5f2fb86a2b2944d0
Abdullah <143931418+abdullah001111@users.noreply.github.com>
2024-02-10 13:12:59 +0600

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,18 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "0"))
+FORCE_SUB_CHANNEL_2 = int(os.environ.get("FORCE_SUB_CHANNEL_2", "0"))
+FORCE_SUB_CHANNEL_3 = int(os.environ.get("FORCE_SUB_CHANNEL_3", "0"))
+FORCE_SUB_CHANNEL_4 = int(os.environ.get("FORCE_SUB_CHANNEL_4", "0"))
+FORCE_SUB_CHANNEL_5 = int(os.environ.get("FORCE_SUB_CHANNEL_5", "0"))
+FORCE_SUB_CHANNEL_6 = int(os.environ.get("FORCE_SUB_CHANNEL_6", "0"))
+FORCE_SUB_CHANNEL_7 = int(os.environ.get("FORCE_SUB_CHANNEL_7", "0"))
+FORCE_SUB_CHANNEL_8 = int(os.environ.get("FORCE_SUB_CHANNEL_8", "0"))
+FORCE_SUB_CHANNEL_9 = int(os.environ.get("FORCE_SUB_CHANNEL_9", "0"))
+FORCE_SUB_CHANNEL_10 = int(os.environ.get("FORCE_SUB_CHANNEL_10", "0"))
+FORCE_SUB_CHANNEL_11 = int(os.environ.get("FORCE_SUB_CHANNEL_11", "0"))
+FORCE_SUB_CHANNEL_12 = int(os.environ.get("FORCE_SUB_CHANNEL_12", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
